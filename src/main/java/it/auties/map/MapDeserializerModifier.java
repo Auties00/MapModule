b0c3772329e2cbc5283eaf39ed977b0992ef3da2
Auties00 <alautiero@gmail.com>
2022-04-15 01:06:20 +0200

    Ready for release

--- src/main/java/it/auties/map/MapDeserializerModifier.java
+++ src/main/java/it/auties/map/MapDeserializerModifier.java
@@ -4,15 +4,11 @@ import com.fasterxml.jackson.databind.BeanDescription;
 import com.fasterxml.jackson.databind.DeserializationConfig;
 import com.fasterxml.jackson.databind.JsonDeserializer;
 import com.fasterxml.jackson.databind.deser.BeanDeserializerModifier;
-import com.fasterxml.jackson.databind.deser.std.StdKeyDeserializer;
 import com.fasterxml.jackson.databind.type.MapType;
 
 public class MapDeserializerModifier extends BeanDeserializerModifier {
     @Override
     public JsonDeserializer<?> modifyMapDeserializer(DeserializationConfig config, MapType type, BeanDescription description, JsonDeserializer<?> deserializer) {
-        var keyDeserializer = StdKeyDeserializer.forType(type.getKeyType().getRawClass());
-        var valueDeserializer = StdKeyDeserializer.forType(type.getContentType().getRawClass());
-        return keyDeserializer != null && valueDeserializer != null ? deserializer
-                : new MapDeserializer<>(type, config.getTypeFactory());
+        return new MapDeserializer<>(type, config.getTypeFactory());
     }
 }
--- src/main/java/it/auties/map/MapSerializer.java
+++ src/main/java/it/auties/map/MapSerializer.java
@@ -6,6 +6,7 @@ import com.fasterxml.jackson.databind.ser.std.StdSerializer;
 
 import java.io.IOException;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class MapSerializer<K, V> extends StdSerializer<Map<K, V>> {
     public MapSerializer() {
@@ -17,6 +18,7 @@ public class MapSerializer<K, V> extends StdSerializer<Map<K, V>> {
         var values = value.entrySet()
                 .stream()
                 .map(entry -> new MapEntry<>(entry.getKey(), entry.getValue()))
+                .distinct()
                 .toList();
         provider.defaultSerializeValue(values, gen);
     }
